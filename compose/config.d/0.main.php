3a84aabb68591b9972facd0592a52fd2e5b807bf
Tharles Andrade <irtharles@gmail.com>
2020-08-08 14:50:41 -0300

    add config files

---
+++ compose/config.d/0.main.php
@@ -0,0 +1,25 @@
+<?php 
+use MapasCulturais\i;
+
+return [
+    'app.siteName' => env('SITE_NAME', 'SECULT - Goiás Lei Aldir Blanc'),
+    'app.siteDescription' => env('SITE_DESCRIPTION', 'Solução para os cadastros da Lei Aldir Blanc'),
+
+    'themes.active' => env('ACTIVE_THEME', 'MapasCulturais\Themes\Gaias'),
+
+    'app.lcode' => env('APP_LCODE', 'pt_BR,es_ES'),
+
+    'namespaces' => array(
+        'MapasCulturais\Themes' => THEMES_PATH,
+        'MapasCulturais\Themes\BaseV1' => THEMES_PATH . 'BaseV1/',
+        'Subsite' => THEMES_PATH . 'Subsite/',
+    ),
+
+    'doctrine.database' => [
+        'host'           => env('DB_HOST', 'db'),
+        'dbname'         => env('DB_NAME', 'mapas'),
+        'user'           => env('DB_USER', 'mapas'),
+        'password'       => env('DB_PASS', 'mapas'),
+        'server_version' => env('DB_VERSION', 11),
+    ]
+];
