23277af39d28feb4d5f3fae9c2324688ec89e12a
Tharles Andrade <irtharles@gmail.com>
2020-08-10 19:56:43 -0300

    disable auth by social media.

--- compose/config.d/authentication.php
+++ compose/config.d/authentication.php
@@ -8,33 +8,35 @@ $_APP_BASE_URL = $prot_part . $host_part;
 
 return [
     'auth.provider' => '\MultipleLocalAuth\Provider',
-    'auth.config' => [], /*array(
+    'auth.config' => array(
         'salt' => env('AUTH_SALT', null),
         'timeout' => '24 hours',
         'strategies' => [
-           'Facebook' => array(
-               'app_id' => env('AUTH_FACEBOOK_APP_ID', null),
-               'app_secret' => env('AUTH_FACEBOOK_APP_SECRET', null),
-               'scope' => env('AUTH_FACEBOOK_SCOPE', 'email'),
-           ),
-
+            'Facebook' => array(
+                'visible' => false,
+                'app_id' => env('AUTH_FACEBOOK_APP_ID', null),
+                'app_secret' => env('AUTH_FACEBOOK_APP_SECRET', null),
+                'scope' => env('AUTH_FACEBOOK_SCOPE', 'email'),
+            ),
             'LinkedIn' => array(
+                'visible' => false,
                 'api_key' => env('AUTH_LINKEDIN_API_KEY', null),
                 'secret_key' => env('AUTH_LINKEDIN_SECRET_KEY', null),
                 'redirect_uri' => $_APP_BASE_URL . 'autenticacao/linkedin/oauth2callback',
                 'scope' => env('AUTH_LINKEDIN_SCOPE', 'r_emailaddress')
             ),
             'Google' => array(
+                'visible' => false,
                 'client_id' => env('AUTH_GOOGLE_CLIENT_ID', null),
                 'client_secret' => env('AUTH_GOOGLE_CLIENT_SECRET', null),
                 'redirect_uri' => $_APP_BASE_URL . 'autenticacao/google/oauth2callback',
                 'scope' => env('AUTH_GOOGLE_SCOPE', 'email'),
             ),
             'Twitter' => array(
+                'visible' => false,
                 'app_id' => env('AUTH_TWITTER_APP_ID', null),
                 'app_secret' => env('AUTH_TWITTER_APP_SECRET', null),
             ),
-
         ]
-    ),*/
+    ),
 ];
